ddef2001dac4b47559289111dd5db2c6b124a240
sandymaramma <edesandraoge@gmail.com>
2023-07-13 13:35:15 +0100

    update

--- App_Project/__init__.py
+++ App_Project/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-	app = Flask(__name__)
-	app.config['SECRET_KEY'] = '081013'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '081013'
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix='/')
 
 
 
@@ -13,8 +15,5 @@ def create_app():
 
 
 
-
-
-
-	return app
+    return app
 
