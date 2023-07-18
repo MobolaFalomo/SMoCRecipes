fcb3c4bdde903539e599790cc1079ec230914587
sandymaramma <edesandraoge@gmail.com>
2023-07-18 08:03:49 +0100

    update

--- App_Project/__init__.py
+++ App_Project/__init__.py
@@ -4,40 +4,30 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '081013'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    app.config.from_object(Config)
 
-   # from .views import views
-    from .auth import auth
-
-    # app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
     
     with app.app_context():
+        app = SQLALchemy(app)
         db.create_all()
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login'
+        login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+from App_Project import views, models
+
+        @login_manager.user_loader
+        def load_user(id):
+            return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
-
+app = create_app()
